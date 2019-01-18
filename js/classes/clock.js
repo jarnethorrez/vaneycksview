fce5d9c99b213f4f193fc21b373dbadde7c8a5ff
jarne thorrez <jarne.thorrez@student.howest.be>
2019-01-18 13:59:19 +0100

    working timer

--- js/classes/clock.js
+++ js/classes/clock.js
@@ -17,8 +17,30 @@ class Clock {
     $timer.classList.add(`timer`);
     $timer.innerText = this.secondsLeft;
 
+    // Add created elements to .clock element
     $clockDiv.appendChild($clock);
     $clockDiv.appendChild($timer);
+  }
+
+  startTimer() {
+    // Start timer on every second
+    const timer = setInterval(() => {
+      //remove 1 second from seconds left & update visual clock
+      this.secondsLeft--;
+      this.updateClock();
+
+      // if no seconds are left, clear timer.
+      if(this.secondsLeft == 0) {
+        clearInterval(timer);
+      }
+
+    }, 1000);
+
+  }
+
+  updateClock() {
+    const $timer = document.querySelector(`.timer`);
+    $timer.innerText = this.secondsLeft;
   }
 
 }
--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const Clock = require('./js/classes/clock');
 const init = () => {
     console.log(`init called from script.js`);
 
-    let c = new Clock(30);
+    let c = new Clock(3);
     c.draw();
+    c.startTimer();
 }
 
 init();
