adc8de583280568937ee68821d2bc3dcd19f4804
jarne thorrez <jarne.thorrez@student.howest.be>
2019-01-18 13:34:26 +0100

    added Cochin + timer

--- js/classes/clock.js
+++ js/classes/clock.js
@@ -1,16 +1,24 @@
 class Clock {
 
-  constructor() {
-    console.log(`hallo ik ben een klokje`);
+  constructor(seconds) {
+    this.secondsLeft = seconds;
   }
 
   draw() {
+    // Get div with class clock from DOM
     const $clockDiv = document.querySelector(`.clock`);
 
+    // create the image element for the Clock
     const $clock = document.createElement(`img`);
     $clock.src="assets/img/clock3.png";
 
+    // create the seconds
+    const $timer = document.createElement(`p`);
+    $timer.classList.add(`timer`);
+    $timer.innerText = this.secondsLeft;
+
     $clockDiv.appendChild($clock);
+    $clockDiv.appendChild($timer);
   }
 
 }
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const Clock = require('./js/classes/clock');
 const init = () => {
     console.log(`init called from script.js`);
 
-    let c = new Clock();
+    let c = new Clock(30);
     c.draw();
 }
 
