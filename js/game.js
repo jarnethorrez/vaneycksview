0e1bde7e988b0e762c08d4df99a4bee673fda93a
jarne thorrez <jarne.thorrez@student.howest.be>
2019-01-22 09:51:47 +0100

    Big commit
    - Basic touch screen features
    - Clickregions
    - Visual feedback on tap
    - Hints as paper

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
 const Clock = require(`../js/classes/Clock`);
 
 let score;
+let $hints;
+let $colorFeedback;
+let time = 30;
 
 const init = () => {
     window.onload = () => {
@@ -8,9 +11,42 @@ const init = () => {
       localStorage.setItem("score", score);
     }
 
-    let c = new Clock(30, timeUp);
+    let c = new Clock(time, timeUp);
     c.draw();
-    //c.startTimer();
+    c.startTimer();
+
+    $details = document.querySelectorAll(`.detail`);
+    $details.forEach($detail => {
+      $detail.addEventListener(`click`, handleDetailClick);
+    });
+
+    $colorFeedback = document.querySelector(`.colorFeedback`);
+    $colorFeedback.addEventListener(`click`, handleColorFeedbackClick);
+
+    $hints = document.querySelectorAll(`.hint`);
+}
+
+const handleColorFeedbackClick = e => {
+  console.log($colorFeedback);
+  $colorFeedback.style.animationName = "feedBackNegative";
+  $colorFeedback.style.animationDuration = "500ms";
+  setTimeout(() => {
+    $colorFeedback.style.animationName = "";
+    $colorFeedback.style.animationDuration = "";
+  }, 500)
+}
+
+const handleDetailClick = e => {
+  const id = e.currentTarget.id;
+  $hints[id].classList.add('hint-guessed');
+  e.currentTarget.parentNode.removeChild(e.currentTarget);
+
+  $colorFeedback.style.animationName = "feedBackPositive";
+  $colorFeedback.style.animationDuration = "500ms";
+  setTimeout(() => {
+    $colorFeedback.style.animationName = "";
+    $colorFeedback.style.animationDuration = "";
+  }, 500)
 }
 
 
